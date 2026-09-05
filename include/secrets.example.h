74dec718e09adb0d4e3f9d1011647c3ab037bcad
Kernel Panic <kernelpanic2015@gmail.com>
2026-09-05 01:58:15 -0300

    docs: add local MQTT secrets template

---
+++ include/secrets.example.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Copy to include/secrets.h for local development. Do not commit secrets.h.
+namespace ProjectSecrets {
+constexpr char MQTT_USERNAME[] = "vhost:username";
+constexpr char MQTT_PASSWORD[] = "change-me";
+}
