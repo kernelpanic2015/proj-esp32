65551a2f6e9698d21c9491e7ca1603c3c570b54c
Kernel Panic <kernelpanic2015@gmail.com>
2026-09-05 04:49:56 -0300

    build: bump firmware to 0.1.2 build 3

--- include/firmware_identity.h
+++ include/firmware_identity.h
@@ -10,8 +10,8 @@ constexpr char MODEL[] = "proj-esp32-35";
 constexpr uint16_t HARDWARE_REVISION = 1;
 
 // Human-readable version plus monotonic build used for update ordering.
-constexpr char VERSION[] = "0.1.1";
-constexpr uint32_t BUILD = 2;
+constexpr char VERSION[] = "0.1.2";
+constexpr uint32_t BUILD = 3;
 constexpr char CHANNEL[] = "dev";
 
 }  // namespace FirmwareIdentity
